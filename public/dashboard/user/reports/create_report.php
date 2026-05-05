b65eaebe81c5e84002ae66e60acb47792028058c
Luigibarte4563 <luigibarte1@gmail.com>
2026-05-05 23:18:29 +0800

    reports

--- public/dashboard/user/reports/create_report.php
+++ public/dashboard/user/reports/create_report.php
@@ -11,7 +11,7 @@
 
 <?php
 session_start();
-require_once __DIR__ . '/../../../src/middleware/requireAuth.php';
+require_once __DIR__ . '/../../../../src/middleware/requireAuth.php';
 $user = requireAuth();
 ?>
 
